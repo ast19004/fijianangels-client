16439edc9d9d394cb598515ae00b28dc79ffa0da
ast19004 <ast19004@byui.edu>
2024-02-08 11:39:21 -0800

    Add Pricing Component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,18 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import CoreValues from "./pages/CoreValues";
+import Pricing from "./pages/Pricing";
 import Services from "./pages/Services";
 
 function App() {
   return (
-    <main>
-      <CoreValues />
-      <Services />
-    </main>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <main>
+        <CoreValues />
+        <Services />
+        <Pricing />
+      </main>
+    </div>
   );
 }
 
