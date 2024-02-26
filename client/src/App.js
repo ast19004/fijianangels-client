00a208579ac5e4633c40fbf88d32ac2e39c6bcb0
ast19004 <ast19004@byui.edu>
2024-02-25 16:32:09 -0800

    add Home Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import logo from "./logo.svg";
 import { Box } from "@mui/material";
 import "./App.css";
 import Footer from "./component/Layout/Footer";
@@ -8,62 +7,13 @@ import Pricing from "./pages/Pricing";
 import Services from "./pages/Services";
 import Staff from "./pages/Staff";
 import Header from "./component/Layout/Header";
-import { useInView } from "react-intersection-observer";
-import { Transition } from "react-transition-group";
-
-import flowerLogo from "./flowerLogo.svg";
+import Home from "./pages/Home";
 
 function App() {
-  const homeBackgroundStyles = {
-    background:
-      "linear-gradient(#fff 25%, rgba(5,150,254, 0.77) , rgb(3,51,66) 95%)",
-    height: "57vw",
-    width: "100vw",
-    position: "absolute",
-    top: "70px",
-    right: "0px",
-    zIndex: "-2",
-  };
-
-  const { ref, inView, entry } = useInView({
-    threshold: 1,
-  });
-
   return (
     <div className="App">
       <Header />
-      <Box
-        sx={{
-          height: "60vw",
-          width: "100vw",
-          position: "sticky",
-          top: "0px",
-          right: "0px",
-          zIndex: "-1",
-          display: "flex",
-          justifyContent: "end",
-        }}
-      >
-        <img
-          style={{
-            display: "block",
-          }}
-          src={flowerLogo}
-          alt="flower logo"
-        />
-      </Box>
-      <Transition in={inView}>
-        {(state) => (
-          <Box
-            ref={ref}
-            sx={{
-              ...homeBackgroundStyles,
-              transition: "opacity 0.75s ease-in-out",
-              opacity: state === "entered" ? 1 : 0,
-            }}
-          ></Box>
-        )}
-      </Transition>
+      <Home />
       <main>
         <CoreValues />
         <Staff />
---
+++ client/src/pages/Home.js
@@ -0,0 +1,60 @@
+import { useInView } from "react-intersection-observer";
+import { Transition } from "react-transition-group";
+
+import flowerLogo from "../flowerLogo.svg";
+import { Box } from "@mui/material";
+
+function Home() {
+  const homeBackgroundStyles = {
+    background:
+      "linear-gradient(#fff 25%, rgba(5,150,254, 0.77) , rgb(3,51,66) 95%)",
+    height: "57vw",
+    width: "100vw",
+    position: "absolute",
+    top: "70px",
+    right: "0px",
+    zIndex: "-2",
+  };
+
+  const { ref, inView, entry } = useInView({
+    threshold: 1,
+  });
+  return (
+    <>
+      <Box
+        sx={{
+          height: "60vw",
+          width: "100vw",
+          position: "sticky",
+          top: "0px",
+          right: "0px",
+          zIndex: "-1",
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <img
+          style={{
+            display: "block",
+          }}
+          src={flowerLogo}
+          alt="flower logo"
+        />
+      </Box>
+      <Transition in={inView}>
+        {(state) => (
+          <Box
+            ref={ref}
+            sx={{
+              ...homeBackgroundStyles,
+              transition: "opacity 0.75s ease-in-out",
+              opacity: state === "entered" ? 1 : 0,
+            }}
+          ></Box>
+        )}
+      </Transition>
+    </>
+  );
+}
+
+export default Home;
