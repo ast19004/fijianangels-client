403a48773cdcb41cb729f3f0842f7ca23d9567b6
ast19004 <ast19004@byui.edu>
2024-02-25 16:23:23 -0800

    Add transitiong to Header background

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,36 @@ import Pricing from "./pages/Pricing";
 import Services from "./pages/Services";
 import Staff from "./pages/Staff";
 import Header from "./component/Layout/Header";
+import { useInView } from "react-intersection-observer";
+import { Transition } from "react-transition-group";
 
 import flowerLogo from "./flowerLogo.svg";
+
 function App() {
+  const homeBackgroundStyles = {
+    background:
+      "linear-gradient(#fff 25%, rgba(5,150,254, 0.77) , rgb(3,51,66) 95%)",
+    height: "57vw",
+    width: "100vw",
+    position: "absolute",
+    top: "70px",
+    right: "0px",
+    zIndex: "-2",
+  };
+
+  const { ref, inView, entry } = useInView({
+    threshold: 1,
+  });
+
   return (
     <div className="App">
       <Header />
       <Box
         sx={{
-          height: "45vw",
+          height: "60vw",
           width: "100vw",
           position: "sticky",
-          top: "70px",
+          top: "0px",
           right: "0px",
           zIndex: "-1",
           display: "flex",
@@ -34,6 +52,18 @@ function App() {
           alt="flower logo"
         />
       </Box>
+      <Transition in={inView}>
+        {(state) => (
+          <Box
+            ref={ref}
+            sx={{
+              ...homeBackgroundStyles,
+              transition: "opacity 0.75s ease-in-out",
+              opacity: state === "entered" ? 1 : 0,
+            }}
+          ></Box>
+        )}
+      </Transition>
       <main>
         <CoreValues />
         <Staff />
