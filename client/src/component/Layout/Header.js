e4de38c8b7ef85febb7b3917ea4c0d435c3420aa
ast19004 <ast19004@byui.edu>
2024-03-09 13:55:29 -0800

    Make Company Text Link to top

--- client/src/component/Layout/Header.js
+++ client/src/component/Layout/Header.js
@@ -9,6 +9,7 @@ import {
   MenuList,
   ClickAwayListener,
   Stack,
+  Button,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
@@ -177,7 +178,20 @@ function Header() {
           )}
         </Popper>
       </Stack>
-      <Typography>FIJIAN ANGELS HOMECARE</Typography>
+      <Button
+        href="/"
+        sx={{
+          margin: 0,
+          fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+          fontWeight: 400,
+          fontSize: "1rem",
+          lineHeight: 1.5,
+          letterSpacing: "0.00938em",
+          color: "#ca4766",
+        }}
+      >
+        FIJIAN ANGELS HOMECARE
+      </Button>
     </header>
   );
 }
