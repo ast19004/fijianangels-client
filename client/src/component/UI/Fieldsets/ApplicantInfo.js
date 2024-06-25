776770b765a70ab635207b5cba740934e5b6554e
ast19004 <ast19004@byui.edu>
2024-06-24 22:33:48 -0700

    Update ApplicantInfo to include application date

--- client/src/component/UI/Fieldsets/ApplicantInfo.js
+++ client/src/component/UI/Fieldsets/ApplicantInfo.js
@@ -6,10 +6,18 @@ import EmploymentEligibility from "../Fieldsets/EmploymentEligibilty";
 import { useContext, useEffect, useState } from "react";
 import { updateInput } from "../../../util/formdata";
 import CustomFieldset from "./styles/CustomFieldset";
+import FormDateInput from "../Inputs/FormDateInput";
 
 const ApplicantInfo = (props) => {
   const applicantCtx = useContext(ApplicantContext);
   const [applicant, setApplicant] = useState(applicantCtx.applicant);
+
+  //Any eligibility data passed over from parent component
+  //is used to set values initially for eligibility inputs
+  //and as the parent changes
+  useEffect(() => {
+    setApplicant(applicantCtx.applicant);
+  }, []);
 
   const handleInputChange = (dataName, data) => {
     updateInput(dataName, data, setApplicant);
@@ -28,6 +36,13 @@ const ApplicantInfo = (props) => {
   return (
     <CustomFieldset>
       <legend>Applicant Information</legend>
+      <FormDateInput
+        id="application_date"
+        htmlFor="application_date"
+        label="Application Date:"
+        onChange={handleInputChange}
+        value={applicant.application_date}
+      />
       <FullName fullName={applicant.fullName} onChange={handleInputChange} />
       <Address address={applicant.address} onChange={handleInputChange} />
       <Contact contact={applicant.contact} onChange={handleInputChange} />
