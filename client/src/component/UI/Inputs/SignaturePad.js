560f99ff3e3a013c7b4978101004e9dc217e13a9
ast19004 <ast19004@byui.edu>
2024-07-08 20:26:14 -0700

    Addd SignatureCanvas component

---
+++ client/src/component/UI/Inputs/SignaturePad.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import SignatureCanvas from "react-signature-canvas";
+import { Box, Button } from "@mui/material";
+
+const SignaturePad = () => {
+  const sigPadRef = useRef(null);
+
+  const clear = () => {
+    sigPadRef.current.clear();
+  };
+
+  const save = () => {
+    if (sigPadRef.current.isEmpty()) {
+      alert("Please provide a signature first.");
+    } else {
+      const dataUrl = sigPadRef.current
+        .getTrimmedCanvas()
+        .toDataUrl("image/png");
+      console.log(dataUrl);
+      //TODO: Send data to parent component
+    }
+  };
+  return (
+    <Box>
+      <SignatureCanvas
+        ref={sigPadRef}
+        penColor="black"
+        canvasProps={{ width: 500, height: 200 }}
+        className="sigCanvas"
+      />
+      <Button onClick={clear}>Clear</Button>
+      <Button onClick={save}>Save</Button>
+    </Box>
+  );
+};
+
+export default SignaturePad;
