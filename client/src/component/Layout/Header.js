3215ba2fa3888e350eb750380648178ce2d3b04a
ast19004 <ast19004@byui.edu>
2024-10-04 12:00:38 -0700

    update main welcome buttons

--- client/src/component/Layout/Header.js
+++ client/src/component/Layout/Header.js
@@ -8,18 +8,7 @@ function Header() {
   return (
     <header>
       <MainNavigation />
-      <Button
-        href="/"
-        sx={{
-          margin: 0,
-          fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-          fontWeight: 400,
-          fontSize: "1rem",
-          lineHeight: 1.5,
-          letterSpacing: "0.00938em",
-          color: "#ca4766",
-        }}
-      >
+      <Button href="/">
         <img className="App-logo" src={logo} alt="Fijian Angels Logo" />
       </Button>
     </header>
