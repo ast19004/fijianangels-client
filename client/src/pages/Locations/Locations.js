79353aee4acd865d60a9ec44ba97eee7f4d9fe4b
ast19004 <ast19004@byui.edu>
2024-10-23 10:43:41 -0700

    Update photos based no resolution and dpi

--- client/src/pages/Locations/Locations.js
+++ client/src/pages/Locations/Locations.js
@@ -19,25 +19,18 @@ const Locations = (props) => {
       <OfficeAddress
         mainOffice
         officeTitle="Main Office"
-        // officeCoverage=""
         officeAddress="8784 Twinberry Way, Elk Grove CA"
       />
       <OfficeAddress
         officeTitle="Golden Gate Regional Center"
-        //         officeCoverage="Covers
-        // Marin County, San Mateo County, & San Francisco County"
         officeAddress="95 Third Street, 2nd Floor, San Francisco 94103"
       />
       <OfficeAddress
         officeTitle="Alta Regional Center"
-        //         officeCoverage="Covers Alpine, Colusa,
-        // El Dorado, Nevada, Placer, Sacramento, Sierra, Sutter, Yolo & Yuba County"
         officeAddress="695 Summit Lakes Way, Galt CA 95632"
       />
       <OfficeAddress
         officeTitle="Regional Center East Bay"
-        //         officeCoverage="Covers Alameda
-        // County & Contra Costa County"
         officeAddress="2001 Addison Street, Berkeley CA"
       />
     </Box>
