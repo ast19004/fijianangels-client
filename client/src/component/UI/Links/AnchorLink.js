72606d05cd6c622d1561584527402ac2d3c1dac7
ast19004 <ast19004@byui.edu>
2025-05-05 10:31:53 -0700

    Fix AnchorLink page change error

--- client/src/component/UI/Links/AnchorLink.js
+++ client/src/component/UI/Links/AnchorLink.js
@@ -3,10 +3,18 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const AnchorLink = (props) => {
+  //States
   const [offset, setOffset] = useState(100);
   const mdMedia = window.innerWidth > 500;
   const lgMedia = window.innerWidth > 1000;
+  const [pageSwitch, setPageSwitch] = useState(false);
+  const [pastHref, setPastHref] = useState('');
 
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Effects
   useEffect(() => {
     let offsetValue;
     if (props.offsetLg && lgMedia) {
@@ -19,19 +27,29 @@ const AnchorLink = (props) => {
     setOffset(offsetValue);
   }, [props.offsetSm, props.offsetMd, props.offsetLg, lgMedia, mdMedia]);
 
-  const targetAnchor = document.querySelector(props.href);
-  const location = useLocation();
-  const navigate = useNavigate();
+  useEffect(() => { 
+    pageSwitch && scrollToHref();
+    setPageSwitch(false);
+    setPastHref('');
+  }, [pageSwitch]);
+
+
+  const scrollToHref = () => {
+    const targetAnchor = document.querySelector(pageSwitch ? pastHref : props.href);
+    if (!targetAnchor) return;
+    const elementPosition = targetAnchor.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.scrollY - offset;
+    window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+  }
 
   const handleClick = (e) => {
     e.preventDefault();
     if (location.pathname !== "/") {
       navigate("/");
+      setPageSwitch(true); 
+      setPastHref(props.href);
     }
-    if (!targetAnchor) return;
-    const elementPosition = targetAnchor.getBoundingClientRect().top;
-    const offsetPosition = elementPosition + window.scrollY - offset;
-    window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+    scrollToHref();
   };
 
   return (
